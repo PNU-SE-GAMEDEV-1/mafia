88c2a035b50d972d2eed71273fcc0a81587febc2
Marko-Shevchuk <markoliubomirshevchykwork@gmail.com>
2024-03-28 22:19:04 +0200

    Fix constant pixel size in some canvases to scale with screen size

--- Assets/CanvasSwitcher.cs
+++ Assets/CanvasSwitcher.cs
@@ -6,7 +6,7 @@ public class CanvasSwitcher : MonoBehaviour
 {
     private Button thisButton;
     public GameObject target;
-    // Start is called before the first frame update
+    // public TMP_InputField input;
     void Start()
     {
         thisButton = GetComponent<Button>();
